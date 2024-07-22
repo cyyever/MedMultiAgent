566e826ea2bfb27bd57ff1b99283b37f08cb9359
cyy <cyyever@outlook.com>
2024-07-22 17:47:04 +0800

    Fix code

--- coordinator.py
+++ coordinator.py
@@ -3,8 +3,8 @@ from agent import DoctorAgent, FacebookCaptionAgent
 
 class Coordinator:
     def __init__(self) -> None:
-        self.default_agent = DoctorAgent
+        self.default_agent = DoctorAgent()
         self.agent = FacebookCaptionAgent()
 
     def invoke(self, message: str) -> str:
-        return self.agent.invoke(message=message)
+        return self.default_agent.invoke(message=message)
