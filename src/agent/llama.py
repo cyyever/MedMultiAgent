0cb405860754c426fb7df33aa3a2dfdbb01018d0
cyy <cyyever@outlook.com>
2025-01-02 11:36:53 +0800

    Fix code

--- src/agent/llama.py
+++ src/agent/llama.py
@@ -1,22 +1,19 @@
-from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableSerializable
+from langchain_ollama import ChatOllama
 
 
 class LLAMAAgent:
-    def __init__(self) -> None:
-        self.__llm: ChatOllama = ChatOllama(model="llama3")
+    def __init__(self, model_name="llama3") -> None:
+        self.__llm: ChatOllama = ChatOllama(model=model_name)
 
     @property
     def prompt_template(self) -> ChatPromptTemplate:
         raise NotImplementedError()
 
     def invoke(self, message: str) -> str:
-        return self.__get_runnable().invoke(self._create_input(message))
+        return self.__get_runnable().invoke({"input": message})
 
     def __get_runnable(self) -> RunnableSerializable:
         return self.prompt_template | self.__llm | StrOutputParser()
-
-    def _create_input(self, message: str) -> dict:
-        return {"input": message}
