9df81e536fb006641d5fa762d06f056d86dde0f6
cyy <cyyever@outlook.com>
2025-01-02 12:07:13 +0800

    Fix code

--- src/coordinator.py
+++ src/coordinator.py
@@ -1,8 +1,17 @@
+import os
 from agent import PredefinedAgent, Role
 
 
 class Coordinator:
     def __init__(self) -> None:
+        role = os.getenv("AGENT_ROLE")
+        if role is not None:
+            for r in Role:
+                if r.lower() == role.lower():
+                    default_role = r
+                    self.agent = PredefinedAgent(role=default_role)
+                    return
+            raise RuntimeError(f"invalid role {role}")
         self.agent = PredefinedAgent(role=Role.Doctor)
 
     def invoke(self, message: str) -> str:
